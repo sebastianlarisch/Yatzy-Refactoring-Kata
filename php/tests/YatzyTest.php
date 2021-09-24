66d8670bee8274587135c1762b6e02119367d8a5
TeufelSL <sebastian.larisch@teufel.de>
2021-09-24 11:26:10 +0200

    clean and add tests

--- php/tests/YatzyTest.php
+++ php/tests/YatzyTest.php
@@ -8,132 +8,171 @@ use Yatzy\Yatzy;
 
 class YatzyTest extends TestCase
 {
+    public function test_ones_score_0(): void
+    {
+        $actual = Yatzy::ones([6, 2, 2, 4, 5]);
+        self::assertSame(0, $actual->getTotal());
+    }
+
+    public function test_ones_score_1(): void
+    {
+        $actual = Yatzy::ones([1, 2, 3, 4, 5]);
+        self::assertSame(1, $actual->getTotal());
+    }
+
+    public function test_twos_score_0(): void
+    {
+        $actual = Yatzy::twos([4, 5, 3, 1, 6]);
+        self::assertSame(0, $actual->getTotal());
+    }
+
+    public function test_twos_score_4(): void
+    {
+        $actual = Yatzy::twos([1, 2, 3, 2, 6]);
+        self::assertSame(4, $actual->getTotal());
+    }
+
+    public function test_twos_score_10(): void
+    {
+        $actual = Yatzy::twos([2, 2, 2, 2, 2]);
+        self::assertSame(10, $actual->getTotal());
+    }
+
+    public function test_threes_score_0(): void
+    {
+        $actual = Yatzy::threes([1, 2, 5, 2, 6]);
+        self::assertSame(0, $actual->getTotal());
+    }
+
+    public function test_threes_score_6(): void
+    {
+        $actual = Yatzy::threes([1, 2, 3, 2, 3]);
+        self::assertSame(6, $actual->getTotal());
+    }
+
+    public function test_fours_score_0(): void
+    {
+        $actual = Yatzy::fours([1, 2, 6, 5, 5]);
+        self::assertSame(0, $actual->getTotal());
+    }
+
+    public function test_fours_score_8(): void
+    {
+        $actual = Yatzy::fours([1, 4, 6, 4, 5]);
+        self::assertSame(8, $actual->getTotal());
+    }
+
+    public function test_fives_score_0(): void
+    {
+        $actual = Yatzy::fives([4, 4, 4, 2, 6]);
+        self::assertSame(0, $actual->getTotal());
+    }
+
+    public function test_fives_score_10(): void
+    {
+        $actual = Yatzy::fives([4, 4, 4, 5, 5]);
+        self::assertSame(10, $actual->getTotal());
+    }
+
+    public function test_sixes_score_0(): void
+    {
+        $actual = Yatzy::sixes([4, 4, 4, 5, 5]);
+        self::assertSame(0, $actual->getTotal());
+    }
+
+    public function test_sixes_score_12(): void
+    {
+        $actual = Yatzy::sixes([4, 4, 4, 6, 6]);
+        self::assertSame(12, $actual->getTotal());
+    }
+
+    public function test_one_pair_score_0(): void
+    {
+        $actual = Yatzy::onePair(3, 4, 1, 5, 6);
+        self::assertSame(0, $actual->getTotal());
+    }
+
+    public function test_one_pair_score_6(): void
+    {
+        $actual = Yatzy::onePair(3, 4, 3, 5, 6);
+        self::assertSame(6, $actual->getTotal());
+    }
+
+    public function test_two_pair_score_0(): void
+    {
+        $actual = Yatzy::twoPair(3, 6, 1, 4, 5);
+        self::assertSame(0, $actual->getTotal());
+    }
+
+    public function test_two_pair_score_16(): void
+    {
+        $actual = Yatzy::twoPair(3, 3, 5, 4, 5);
+        self::assertSame(16, $actual->getTotal());
+    }
+
+    public function test_three_of_a_kind_score_0(): void
+    {
+        $actual = Yatzy::threeOfAKind(3, 3, 2, 4, 5);
+        self::assertSame(0, $actual->getTotal());
+    }
+
+    public function test_three_of_a_kind_score_9(): void
+    {
+        $actual = Yatzy::threeOfAKind(3, 3, 3, 4, 5);
+        self::assertSame(9, $actual->getTotal());
+    }
+
+    public function test_small_straight_score_0(): void
+    {
+        $actual = Yatzy::smallStraight(1, 2, 6, 4, 5);
+        self::assertSame(0, $actual->getTotal());
+    }
+
+    public function test_small_straight_score_15(): void
+    {
+        $actual = Yatzy::smallStraight(1, 2, 3, 4, 5);
+        self::assertSame(15, $actual->getTotal());
+    }
+
+    public function test_large_straight_score_0(): void
+    {
+        $actual = Yatzy::largeStraight(6, 2, 1, 4, 5);
+        self::assertSame(0, $actual->getTotal());
+    }
+
+    public function test_large_straight_score_20(): void
+    {
+        $actual = Yatzy::largeStraight(6, 2, 3, 4, 5);
+        self::assertSame(20, $actual->getTotal());
+    }
+
+    public function test_full_house_score_0(): void
+    {
+        $actual = Yatzy::fullHouse(6, 2, 1, 2, 6);
+        self::assertSame(0, $actual->getTotal());
+    }
+
+    public function test_full_house_score_18(): void
+    {
+        $actual = Yatzy::fullHouse(6, 2, 2, 2, 6);
+        self::assertSame(18, $actual->getTotal());
+    }
+
     public function test_chance_scores_sum_of_all_dice(): void
     {
-        $expected = 15;
         $actual = Yatzy::chance(2, 3, 4, 5, 1);
-        self::assertSame($expected, $actual->getTotal());
+        self::assertSame(15, $actual->getTotal());
     }
 
     public function test_yatzy_scores_50(): void
     {
-        $expected = 50;
         $actual = Yatzy::yatzy([4, 4, 4, 4, 4]);
-        self::assertSame($expected, $actual->getTotal());
-        //self::assertSame(50, Yatzy::yatzyScore([6, 6, 6, 6, 6]));
-        //self::assertSame(0, Yatzy::yatzyScore([6, 6, 6, 6, 3]));
+        self::assertSame(50, $actual->getTotal());
     }
 
-    public function test_1s(): void
+    public function test_yatzy_scores_0(): void
     {
-        $actual = Yatzy::ones([1, 2, 3, 4, 5]);
-        self::assertSame(1, $actual->getTotal());
-//        self::assertSame(2, Yatzy::ones(1, 2, 1, 4, 5));
-//        self::assertSame(0, Yatzy::ones(6, 2, 2, 4, 5));
-//        self::assertSame(4, Yatzy::ones(1, 2, 1, 1, 1));
-    }
-
-    public function test_2s(): void
-    {
-        $actual = Yatzy::twos([1, 2, 3, 2, 6]);
-        self::assertSame(4, $actual->getTotal());
-
-        //self::assertSame(10, Yatzy::twos(2, 2, 2, 2, 2));
-    }
-
-    public function test_threes(): void
-    {
-        $actual = Yatzy::threes([1, 2, 3, 2, 3]);
-        self::assertSame(6, $actual->getTotal());
-        //self::assertSame(12, Yatzy::threes(2, 3, 3, 3, 3));
-    }
-
-    public function test_fours_test(): void
-    {
-        $actual = Yatzy::fours([4, 4, 4, 5, 5]);
-        self::assertSame(12, $actual->getTotal());
-
-//        self::assertSame(12, (new Yatzy(4, 4, 4, 5, 5))->fours());
-//        self::assertSame(8, (new Yatzy(4, 4, 5, 5, 5))->fours());
-//        self::assertSame(4, (new Yatzy(4, 5, 5, 5, 5))->fours());
-    }
-
-    public function test_fives(): void
-    {
-        $actual = Yatzy::fives([4, 4, 4, 5, 5]);
-        self::assertSame(10, $actual->getTotal());
-
-//        self::assertSame(10, (new Yatzy(4, 4, 4, 5, 5))->Fives());
-//        self::assertSame(15, (new Yatzy(4, 4, 5, 5, 5))->Fives());
-//        self::assertSame(20, (new Yatzy(4, 5, 5, 5, 5))->Fives());
-    }
-
-    public function sixes_test(): void
-    {
-        $actual = Yatzy::sixes([4, 4, 4, 5, 5]);
+        $actual = Yatzy::yatzy([4, 4, 4, 4, 3]);
         self::assertSame(0, $actual->getTotal());
-
-//        self::assertSame(0, (new Yatzy(4, 4, 4, 5, 5))->sixes());
-//        self::assertSame(6, (new Yatzy(4, 4, 6, 5, 5))->sixes());
-//        self::assertSame(18, (new Yatzy(6, 5, 6, 6, 5))->sixes());
-    }
-
-    public function test_one_pair(): void
-    {
-        $actual = Yatzy::onePair(3, 4, 3, 5, 6);
-        self::assertSame(6, $actual->getTotal());
-
-//        self::assertSame(6, Yatzy::score_pair(3, 4, 3, 5, 6));
-//        self::assertSame(10, Yatzy::score_pair(5, 3, 3, 3, 5));
-//        self::assertSame(12, Yatzy::score_pair(5, 3, 6, 6, 5));
-    }
-
-    public function test_two_Pair(): void
-    {
-        $actual = Yatzy::twoPair(3, 3, 5, 4, 5);
-        self::assertSame(16, $actual->getTotal());
-
-//        self::assertSame(16, Yatzy::two_pair(3, 3, 5, 4, 5));
-//        self::assertSame(18, Yatzy::two_pair(3, 3, 6, 6, 6));
-//        self::assertSame(0, Yatzy::two_pair(3, 3, 6, 5, 4));
-    }
-
-    public function test_three_of_a_kind(): void
-    {
-        $actual = Yatzy::threeOfAKind(3, 3, 3, 4, 5);
-        self::assertSame(9, $actual->getTotal());
-
-//        self::assertSame(9, Yatzy::three_of_a_kind(3, 3, 3, 4, 5));
-//        self::assertSame(15, Yatzy::three_of_a_kind(5, 3, 5, 4, 5));
-//        self::assertSame(9, Yatzy::three_of_a_kind(3, 3, 3, 2, 1));
-    }
-
-    public function test_smallStraight(): void
-    {
-        $actual = Yatzy::smallStraight(1, 2, 3, 4, 5);
-        self::assertSame(15, $actual->getTotal());
-
-//        self::assertSame(15, Yatzy::smallStraight(1, 2, 3, 4, 5));
-//        self::assertSame(15, Yatzy::smallStraight(2, 3, 4, 5, 1));
-//        self::assertSame(0, Yatzy::smallStraight(1, 2, 2, 4, 5));
-    }
-
-    public function test_largeStraight(): void
-    {
-        $actual = Yatzy::largeStraight(6, 2, 3, 4, 5);
-        self::assertSame(20, $actual->getTotal());
-
-//        self::assertSame(20, Yatzy::largeStraight(6, 2, 3, 4, 5));
-//        self::assertSame(20, Yatzy::largeStraight(2, 3, 4, 5, 6));
-//        self::assertSame(0, Yatzy::largeStraight(1, 2, 2, 4, 5));
-    }
-
-    public function test_fullHouse(): void
-    {
-        $actual = Yatzy::fullHouse(6, 2, 2, 2, 6);
-        self::assertSame(18, $actual->getTotal());
-
-//        self::assertSame(18, Yatzy::fullHouse(6, 2, 2, 2, 6));
-//        self::assertSame(0, Yatzy::fullHouse(2, 3, 4, 5, 6));
     }
 }
